ec8ed84e57d7dbe21ad63446fd5db354ed4197a9
peanut-lover <jihoson94@gmail.com>
2022-11-07 19:40:17 +0900

    :sparkles: Add Class Component

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 /* @jsx createElement */
-import { render, createElement } from "./react";
+import { render, createElement, Component} from "./react";
 
-function Title(props) {
-  return <h1>{props.children}</h1>
-} 
+class Title extends Component {
+  render() {
+    return <h1>{this.props.children}</h1>
+  }
+}
+ 
 function Item(props) {
   return <li style={`color: ${props.color}`}>{props.children}</li>
 }
-const vdom = <p>
+const App = () => <p>
   <Title>React 잘 만들기</Title>
   <ul>
     <Item color="red">첫 번째 아이템</Item>
@@ -16,4 +19,4 @@ const vdom = <p>
   </ul>
 </p>
 
-render(vdom, document.querySelector("#root"));
+render(<App />, document.querySelector("#root"));
--- src/react.js
+++ src/react.js
@@ -1,3 +1,12 @@
+
+export class Component {
+  
+  constructor(props) {
+    this.props = props;
+  }
+}
+
+
 export function createDOM(node) {
   if (typeof node === "string") {
     return document.createTextNode(node);
@@ -13,19 +22,45 @@ export function createDOM(node) {
   return element;
 }
 
+function makeProps(props, children) {
+  return { ...props, children: children.length === 1 ? children[0] : children };
+}
+
 export function createElement(tag, props , ...children) {
   if (typeof tag === "function") {
-    if (children.length > 0) { 
-      return tag({...props, children: children.length === 1? children[0]: children});
+    // TODO: 원래 Symbol로 비교해야함.
+    if (tag.prototype instanceof Component) {
+      // TODO: 상태가 변경될떄마다 매번 createElement가 호출되기에 instance가 새로 생성되는 상태
+      // 실제로는 instance를 기억하고 render()만 호출하는 형태로 처리될 것입니다. 
+      const instance = new tag(makeProps(props, children));
+      return instance.render();
     } else {
-      return tag(props);
+      if (children.length > 0) { 
+        return tag(makeProps(props, children));
+      } else {
+        return tag(props);
+      }
     }
+    
   } else {
     props = props ?? {};
     return { tag, props, children };
   }
 }
 
-export function render(vdom, container) {
-  container.appendChild(createDOM(vdom));
-}
+// export function render(vdom, container) {
+//   container.appendChild(createDOM(vdom));
+// }
+
+export const render = (function () {
+  let prevDom = null;
+  return function (vdom, container) {
+    if (prevDom === null) {
+      prevDom = vdom
+    }
+    
+    // diff
+
+    container.appendChild(createDOM(vdom));
+  }
+})();
