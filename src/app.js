a33529b65a06935e4a7cf1f7bd501b0f1f776dc3
peanut-lover <jihoson94@gmail.com>
2022-11-07 18:25:27 +0900

    :sparkles: Add Function Component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
+/* @jsx createElement */
 import { render, createElement } from "./react";
 
-const vdom = createElement("p", {},
-  createElement("h1", {}, "React 만들기"),
-  createElement("ul", {},
-    createElement("li", { style: "color: red",}, "첫 번째 아이템"),
-    createElement("li", {style: "color: blue",}, "두 번째 아이템"),
-    createElement("li", {style: "color: green",}, "세 번째 아이템"),
-  ),
-)
+function Title(props) {
+  return <h1>{props.children}</h1>
+} 
+function Item(props) {
+  return <li style={`color: ${props.color}`}>{props.children}</li>
+}
+const vdom = <p>
+  <Title>React 잘 만들기</Title>
+  <ul>
+    <Item color="red">첫 번째 아이템</Item>
+    <Item color="green">두 번째 아이템</Item>
+    <Item color="blue">세 번째 아이템</Item>
+  </ul>
+</p>
+
 render(vdom, document.querySelector("#root"));
